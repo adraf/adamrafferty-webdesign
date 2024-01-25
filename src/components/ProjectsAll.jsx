853d5cbb291c83e314cb00fc789523235d54be7f
adraf <adraf@me.com>
2024-01-25 18:11:38 +0000

    more mobile styling, added in a modal with a carousel to single image page

--- src/components/ProjectsAll.jsx
+++ src/components/ProjectsAll.jsx
@@ -18,31 +18,64 @@ export default function ProjectsAll() {
     })
   }
 
+  const prevIcon = <svg id='prevIcon' xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
+    <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+  </svg>
+  const nextIcon = <svg id='nextIcon' xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+    <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+  </svg>
+
   return (
-    <section className='main-projects'>
-      <Carousel className='d-flex'>
+    <>
+      <section className='main-projects'>
+        <Carousel className='d-flex' prevIcon={prevIcon} nextIcon={nextIcon}>
+          {projectsArray.map(project => {
+            const { id:projectId, title, languages, projectImages } = project
+            return (
+              <Carousel.Item key={projectId} className='w-100 h-100 p-3 project-container-carousel'  xl={3} md={6} style={{ backgroundImage: `url(${projectImages[0]})` }}>
+                <Link to={`/project/${projectId}/`} onClick={() => getIndProject(projectId)}>
+                  <div className='carousel-information'>
+                    <Carousel.Caption className='d-flex flex-column align-items-start'>
+                      <h2>{title}</h2>
+                      <div className='skills-badge-container'>
+                      {languages.map(language => {
+                        return (
+                          <Badge key={projectId+language} className='skills-badge'>{language}</Badge>
+                        )
+                      })}
+                    </div>
+                  </Carousel.Caption>
+                  </div>
+                </Link>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
+      </section>
+      <section className='main-projects-mobile'>
         {projectsArray.map(project => {
           const { id:projectId, title, languages, projectImages } = project
           return (
-            <Carousel.Item key={projectId} className='w-100 h-100 p-3 project-container-carousel' style={{ backgroundImage: `url(${projectImages[0]})` }}>
-              <Link to={`/project/${projectId}/`} onClick={() => getIndProject(projectId)}>
+            <Link className='mobile-project-sections' to={`/project/${projectId}/`} onClick={() => getIndProject(projectId)} key={projectId}>
+              <section  className='project-container-carousel' style={{ backgroundImage: `url(${projectImages[0]})` }}>
+              </section>
                 <div className='carousel-information'>
-                  <Carousel.Caption className='d-flex flex-column align-items-start'>
+                  <div className='d-flex flex-column align-items-start'>
                     <h2>{title}</h2>
                     <div className='skills-badge-container'>
-                    {languages.map(language => {
-                      return (
-                        <Badge key={projectId+language} className='skills-badge'>{language}</Badge>
-                      )
-                    })}
+                      {languages.map(language => {
+                        return (
+                          <Badge key={projectId+language} className='skills-badge'>{language}</Badge>
+                        )
+                      })}
+                    </div>
                   </div>
-                </Carousel.Caption>
                 </div>
-              </Link>
-            </Carousel.Item>
+        
+            </Link>
           )
         })}
-      </Carousel>
-    </section>
+      </section>
+    </>
   )
 }
